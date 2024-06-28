5d764a5ab464928d40e53a7f43e914b92dca0c8e
Antony Jaison <antonyjaison639@gmail.com>
2024-06-28 18:32:05 +0530

    completed input text box with commands

--- src/app/chat/_components/TextBox.tsx
+++ src/app/chat/_components/TextBox.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from 'react'
 import { Input } from 'react-chat-elements'
-import { Send } from 'lucide-react'
+import { MessageCircle, Send } from 'lucide-react'
 import { linearGradient } from '@/lib/constants'
 import "react-chat-elements/dist/main.css"
 import {
@@ -14,29 +14,46 @@ import {
 } from "@/components/ui/command"
 import { cn } from '@/lib/utils'
 import { UserRoundPlus, Sparkles } from 'lucide-react'
+import { ChatOptionsType, ChatSuggestionsType } from '@/lib/types'
 
 const TextBox = () => {
     const [inputText, setInputText] = useState('')
     const [showCommands, setShowCommands] = useState(false)
 
-    const suggestions = [
+    const [suggestion, setSuggestion] = useState<ChatSuggestionsType>(null)
+    const [option, setOption] = useState<ChatOptionsType>(null)
+
+    const [selected, setSelected] = useState<React.ReactElement | null>(null)
+
+    type SuggestionType = {
+        id: number
+        title: string
+        icon: JSX.Element
+        color: string
+        option: ChatSuggestionsType
+    }
+
+    const suggestions: SuggestionType[] = [
         {
             id: 1,
             title: 'Generate notes',
-            icon: <Sparkles size={18} color='#F8BD00' />,
-            color: "#F8BD00"
+            icon: <Sparkles size={18} className='icon-hover-scale' color='#F8BD00' />,
+            color: "#F8BD00",
+            option: "generate_notes"
         },
         {
             id: 2,
             title: 'Create flashcards',
-            icon: <Sparkles size={18} color='#3A78F2' />,
-            color: "#3A78F2"
+            icon: <Sparkles size={18} className='icon-hover-scale' color='#3A78F2' />,
+            color: "#3A78F2",
+            option: "create_flashcards"
         },
         {
             id: 3,
             title: '“Explain it to me like I’m 5”',
-            icon: <Sparkles size={18} color='#FF7F50' />,
-            color: "#FF7F50"
+            icon: <Sparkles size={18} className='icon-hover-scale' color='#FF7F50' />,
+            color: "#FF7F50",
+            option: "explain"
         },
     ]
 
@@ -46,7 +63,59 @@ const TextBox = () => {
         } else {
             setShowCommands(false)
         }
+
+        if (option) {
+            setShowCommands(false)
+        }
+
+        if (suggestion) {
+            setShowCommands(false)
+        }
+
+        if (inputText === '') {
+            setShowCommands(false)
+            setSelected(null)
+            setOption(null)
+            setSuggestion(null)
+        }
     }, [inputText])
+
+    const handleOptionChange = (option: ChatOptionsType | null) => {
+        setOption(option)
+        setSuggestion(null)
+        setShowCommands(false)
+    }
+
+    const handleSuggestionChange = (suggestion: ChatSuggestionsType | null) => {
+        setSuggestion(suggestion)
+        setOption(null)
+        setShowCommands(false)
+    }
+
+    useEffect(() => {
+        switch (suggestion) {
+            case "generate_notes":
+                setSelected(<button style={{ boxShadow: "0px 0px 4px 0px #F8BD00" }} className=' text-[#F8BD00] font-inter text-sm font-medium bg-white rounded-md px-3 py-2'>Generate notes</button>)
+                break
+            case "create_flashcards":
+                setSelected(<button style={{ boxShadow: "0px 0px 4px 0px #3A78F2" }} className=' text-[#3A78F2] font-inter text-sm font-medium bg-white rounded-md px-3 py-2'>Create flashcards</button>)
+                break
+            case "explain":
+                setSelected(<button style={{ boxShadow: "0px 0px 4px 0px #FF7F50" }} className=' text-[#FF7F50] font-inter text-sm font-medium bg-white rounded-md px-3 py-2'>“Explain it to me like I’m 5”</button>)
+                break
+        }
+    }, [suggestion])
+
+    useEffect(() => {
+        switch (option) {
+            case "invite_friends":
+                setSelected(<button style={{ boxShadow: "0px 0px 4px 0px #334155" }} className=' text-[#334155] font-inter text-sm font-medium bg-white rounded-md px-3 py-2'>Invite friends</button>)
+                break
+            case "chat_with_friends":
+                setSelected(<button style={{ boxShadow: "0px 0px 4px 0px #334155" }} className=' text-[#334155] font-inter text-sm font-medium bg-white rounded-md px-3 py-2'>Chat with friends</button>)
+                break
+        }
+    }, [option])
 
     return (
         <div className='w-full flex justify-between h-fit items-center gap-3 px-5'>
@@ -56,20 +125,26 @@ const TextBox = () => {
                         <Command className='rounded-b-none'>
                             <CommandList>
                                 <CommandGroup className='font-inter text-[#334155] text-xl font-medium'>
-                                    <p className='font-inter text-[#334155] text-base font-semibold pt-1 pl-1 pb-1'>Functions</p>
-                                    <CommandItem>
-                                        <div className='flex h-fit items-center gap-3'>
-                                            <UserRoundPlus size={18} />
+                                    <p className='font-inter text-[#334155] text-base font-semibold pt-1 pl-1 pb-1'>Options</p>
+                                    <CommandItem className='div-hover-scale cursor-pointer'>
+                                        <div onClick={() => handleOptionChange("invite_friends")} className='flex h-fit items-center gap-3'>
+                                            <UserRoundPlus size={18} className='icon-hover-scale' />
                                             Invite friends
+                                        </div>
+                                    </CommandItem>
+                                    <CommandItem className='div-hover-scale cursor-pointer'>
+                                        <div onClick={() => handleOptionChange("chat_with_friends")} className='flex h-fit items-center gap-3'>
+                                            <MessageCircle size={18} className='icon-hover-scale' />
+                                            Chat with a friends
                                         </div>
                                     </CommandItem>
                                 </CommandGroup>
                                 <CommandSeparator />
                                 <CommandGroup className='font-inter text-[#334155] text-xl font-medium'>
                                     <p className='font-inter text-[#334155] text-base font-semibold pt-1 pl-1 pb-1'>Suggestions</p>
-                                    {suggestions.map((suggestion) => (
-                                        <CommandItem key={suggestion.id}>
-                                            <div style={{ color: suggestion.color }} className='flex h-fit items-center gap-3'>
+                                    {suggestions.map((suggestion: SuggestionType) => (
+                                        <CommandItem key={suggestion.id} className='div-hover-scale cursor-pointer'>
+                                            <div onClick={() => handleSuggestionChange(suggestion.option)} style={{ color: suggestion.color }} className='flex h-fit items-center gap-3'>
                                                 {suggestion.icon}
                                                 {suggestion.title}
                                             </div>
@@ -80,11 +155,27 @@ const TextBox = () => {
                         </Command>
                     </div>
                 )}
+
+                {selected && (
+                    <div className='w-full px-[1px]'>
+                        <Command className='rounded-b-none'>
+                            <CommandList>
+                                <CommandGroup className='font-inter text-[#334155] text-xl font-medium'>
+                                    <CommandItem disabled>
+                                        {selected}
+                                    </CommandItem>
+                                </CommandGroup>
+                            </CommandList>
+                        </Command>
+                    </div>
+                )}
+
                 <Input
                     placeholder="Type your message here..."
                     multiline={true}
                     className={cn('p-3 border border-[#EBEDF0] rounded-md font-inter font-normal text-[#0F172A]',
-                        showCommands && "rounded-t-none"
+                        showCommands && "rounded-t-none",
+                        selected && "rounded-t-none"
                     )}
                     value={inputText}
                     onChange={(e: any) => setInputText(e.target.value)}
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,3 +3,7 @@ export type CollaboratorType = {
     name: string
     avatar: string
 }
+
+export type ChatOptionsType = 'invite_friends' | 'chat_with_friends' | null
+
+export type ChatSuggestionsType = 'generate_notes' | 'create_flashcards' | 'explain' | null
