1291618c4f5f11c5213364d3196d53d7fb7ca0fc
hashiq300 <mohdhashique10@gmail.com>
2024-07-01 16:39:41 +0530

    Individual page

--- src/app/chat/[chatId]/components.tsx
+++ src/app/chat/[chatId]/components.tsx
@@ -9,33 +9,39 @@ import UploadedFile from '../_components/UploadedFile'
 import { ChatDetails } from '@/lib/types'
 
 type IndividualChatPageProps = {
-    chatDetails: {
-        data: ChatDetails | null
-        error: string | null
-    }
-}
+  chatDetails: ChatDetails | null;
+};
 
-export const IndividualChatPage = ({ chatDetails } : IndividualChatPageProps) => {
-    const { messages, input, handleInputChange, handleSubmit, setMessages } = useChat({
-        initialMessages: chatDetails.data?.messages as any
+export const IndividualChatPage = ({
+  chatDetails,
+}: IndividualChatPageProps) => {
+  const { messages, input, handleInputChange, handleSubmit, setMessages } =
+    useChat({
+      initialMessages: chatDetails?.messages as any,
     });
-    // console.log(chatDetails.data?.messages)
+  console.log(chatDetails?.messages);
 
-    console.log(messages)
+  console.log(messages);
 
+  return (
+    <div className=" ml-5 h-full">
+      <div className=" h-3/4 flex justify-center items-center">
+        <ChatSection messages={messages} />
+      </div>
 
-    return (
-        <div className=' ml-5 h-full'>
-
-            <div className=' h-3/4 flex justify-center items-center'>
-                <ChatSection messages={messages} />
-            </div>
-
-            <div className='h-1/4 flex flex-col gap-8 items-center justify-end'>
-                <TextBox setMessages={setMessages} input={input} handleInputChange={handleInputChange} handleSubmit={handleSubmit} />
-                <p className=' text-[#7D7D7D] font-inter text-sm font-normal'>Free development preview. Rune may produce inaccurate information about people, places or facts.</p>
-            </div>
-        </div>
-    )
-}
+      <div className="h-1/4 flex flex-col gap-8 items-center justify-end">
+        <TextBox
+          setMessages={setMessages}
+          input={input}
+          handleInputChange={handleInputChange}
+          handleSubmit={handleSubmit}
+        />
+        <p className=" text-[#7D7D7D] font-inter text-sm font-normal">
+          Free development preview. Rune may produce inaccurate information
+          about people, places or facts.
+        </p>
+      </div>
+    </div>
+  );
+};
 
